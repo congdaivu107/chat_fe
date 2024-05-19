b9261a3b7d55bbf617cd5b7ee1387370f1e66a3a
iamtun <letuan19431791.iuh@gmail.com>
2024-05-19 19:06:35 +0700

    handle send text message

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -8,6 +8,14 @@ class MessageService {
     async getMessageListByConversationId(id) {
         return this.req.get(`/messages/${id}?page=1&limit=5`);
     }
+
+    async sendMessage(formData) {
+        return this.req.post(`/messages`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        });
+    }
 }
 
 const messageService = new MessageService(request);
